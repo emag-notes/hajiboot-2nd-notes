ba0e5d3dc5a043ae42ccbb175fa572e507367d8a
emag <lanabe.lanabe@gmail.com>
2018-02-12 20:31:58 +0900

    Add chap03

---
+++ chap03/hajiboot-rest/src/main/java/com/example/domain/Customer.java
@@ -0,0 +1,26 @@
+package com.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "customers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer implements Serializable {
+
+  @Id @GeneratedValue
+  private Integer id;
+
+  @Column(nullable = false)
+  private String firstName;
+
+  @Column(nullable = false)
+  private String lastName;
+
+}
