208fa7944b6de0ab24b68726326beb8dbf0f3261
emag <lanabe.lanabe@gmail.com>
2018-02-13 20:26:10 +0900

    Add chap05

--- chap01/hajiboot/src/test/java/com/example/hajiboot/HajibootApplicationTests.java
+++ chap01/hajiboot/src/test/java/com/example/hajiboot/HajibootApplicationTests.java
@@ -2,15 +2,32 @@ package com.example.hajiboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HajibootApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	@LocalServerPort
+  int port;
+
+  @Autowired
+  TestRestTemplate restTemplate;
+
+  @Test
+  public void contextLoads() {
+    ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port, String.class);
+
+    assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(response.getBody()).isEqualTo("Hello world!");
+  }
 
 }
